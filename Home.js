b585adef1523ebc8bf6f46f0071d3b9456fdb2f9
Sean Diaz <116705898+SeanPlatinum@users.noreply.github.com>
2023-08-25 00:54:59 -0400

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,10 @@
+export default function Home () {
+    return(
+        <div className="home">
+            <h1 className="hello">SEAN DIAZ</h1>
+       
+        </div>
+        
+        
+    )
+}
