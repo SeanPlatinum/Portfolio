43ef16388d434128f8d692e1b0f61925229d9ac1
Sean Diaz <116705898+SeanPlatinum@users.noreply.github.com>
2023-09-01 19:36:56 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import './styles.css';
+import './components/work.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
