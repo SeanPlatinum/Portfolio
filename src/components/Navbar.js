38d32e512ce8e120ff67dc6fbb967f1631fe6b3e
Sean Diaz <116705898+SeanPlatinum@users.noreply.github.com>
2023-09-02 00:37:58 -0400

    finished first version of portfolio

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,33 @@
 import logo from '../assets/images/logo (1).png';
 
-const Navbar = () => {
+const Navbar = ({ onNavLinkClick }) => {
+    const smoothScroll = (id) => {
+        onNavLinkClick();
+        setTimeout(() => {
+            document.getElementById(id).scrollIntoView({ 
+                behavior: 'smooth'
+            });
+        }, 0);
+    }
 
-    return (<nav className='nav'>
-        <a className='Title'><img src = {logo} width={80}/></a>
-        <ul>
-            <li>
-                <a href = '#Projects'>Projects</a>
-            </li>
-            <li>
-                <a href = '#about'>About</a>
-            </li>
-            <li>
-                <a href = '#contact'>Contact</a>
-            </li>
-        </ul>
-    </nav>)
 
+
+    return (
+        <nav className='nav'>
+            <a className='Title'><img src={logo} width={80} /></a>
+            <ul>
+                <li>
+                    <a onClick={() => smoothScroll('Projects')}>Projects</a>
+                </li>
+                <li>
+                    <a onClick={() => smoothScroll('About')}>About</a>
+                </li>
+                <li>
+                    <a onClick={() => smoothScroll('Contact')}>Contact</a>
+                </li>
+            </ul>
+        </nav>
+    )
 }
 
 export default Navbar;
